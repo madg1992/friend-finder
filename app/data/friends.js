866980a69d360be71ca5fc7628f0e9bd92037272
Mark Gallardo <mrk_gllrd@yahoo.com>
2019-12-10 15:10:11 -0600

    added friend list data

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friends = [
+    {
+        name: "Sulli",
+        photo: "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1560&q=80",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "3",
+            "4",
+            "3",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
+    },
+    {
+        name: "Charlie",
+        photo: "https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
+        scores: [
+            "3",
+            "1",
+            "2",
+            "4",
+            "4",
+            "1",
+            "1",
+            "2",
+            "4",
+            "3"
+        ]
+    },
+    {
+        name: "Bobby",
+        photo: "https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=60",
+        scores: [
+            "3",
+            "3",
+            "3",
+            "2",
+            "2",
+            "1",
+            "4",
+            "4",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Sulli",
+        photo: "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1560&q=80",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "3",
+            "4",
+            "3",
+            "4",
+            "4",
+            "4",
+            "2"
+        ]
+    },
+    {
+        name: "Chelsea",
+        photo: "https://images.unsplash.com/photo-1559190394-df5a28aab5c5?ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=60",
+        scores: [
+            "2",
+            "2",
+            "2",
+            "5",
+            "5",
+            "4",
+            "4",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Kaleb",
+        photo: "https://images.unsplash.com/photo-1566624790190-511a09f6ddbd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80",
+        scores: [
+            "2",
+            "3",
+            "2",
+            "3",
+            "2",
+            "3",
+            "4",
+            "4",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Harry",
+        photo: "https://images.unsplash.com/photo-1528579779874-d308ed01d197?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
+        scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "1",
+            "1",
+            "1",
+            "5",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Alexa",
+        photo: "https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
+        scores: [
+            "4",
+            "5",
+            "1",
+            "4",
+            "1",
+            "2",
+            "4",
+            "4",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Dylan",
+        photo: "https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
+        scores: [
+            "4",
+            "3",
+            "1",
+            "5",
+            "2",
+            "2",
+            "1",
+            "4",
+            "3",
+            "1"
+        ]
+    },
+    {
+        name: "Eddy",
+        photo: "https://images.unsplash.com/photo-1504826260979-242151ee45b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60",
+        scores: [
+            "4",
+            "1",
+            "1",
+            "3",
+            "5",
+            "2",
+            "1",
+            "4",
+            "5",
+            "4"
+        ]
+    },
+];
+
+module.exports = friends;
